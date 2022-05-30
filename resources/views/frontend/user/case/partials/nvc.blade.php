8042071ad8855f5eb524bbdf6cb384edc145f764
rakharamadhana <rakha.ramadhana@yahoo.com>
2022-05-30 14:44:31 +0800

    Add more users & nvc tag

--- resources/views/frontend/user/case/partials/nvc.blade.php
+++ resources/views/frontend/user/case/partials/nvc.blade.php
@@ -10,6 +10,7 @@
 
     <div class="mb-3 task">
         <div class="mt-3 mb-3">
+            <span class="sel-sub-header h5" style="background-color: #88df6c;">@lang('Observation')</span>
             <label for="exampleFormControlTextarea1" class="form-label">@lang('Please share what you think the protagonist has observed from the highlighted sentence (Observation)')</label>
             <textarea class="form-control" id="exampleFormControlTextarea1" name="nvc_1" rows="4">@isset($student_answer){{ $student_answer->nvc_1 }}@endisset</textarea>
         </div>
@@ -17,6 +18,7 @@
 
     <div class="mb-3 task">
         <div class="mt-3 mb-3">
+            <span class="sel-sub-header h5" style="background-color: #88df6c;">@lang('Perceives')</span>
             <label for="exampleFormControlTextarea1" class="form-label">@lang('Please share what you think the protagonist has observed from the highlighted sentence (Perceives)')</label>
             <textarea class="form-control" id="exampleFormControlTextarea1" name="nvc_2" rows="4">@isset($student_answer){{ $student_answer->nvc_2 }}@endisset</textarea>
         </div>
@@ -24,6 +26,7 @@
 
     <div class="mb-3 task">
         <div class="mt-3 mb-3">
+            <span class="sel-sub-header h5" style="background-color: #88df6c;">@lang('Needs')</span>
             <label for="exampleFormControlTextarea1" class="form-label">@lang('Please share what you think the protagonist has observed from the highlighted sentence (Needs)')</label>
             <textarea class="form-control" id="exampleFormControlTextarea1" name="nvc_3" rows="4">@isset($student_answer){{ $student_answer->nvc_3 }}@endisset</textarea>
         </div>
@@ -31,6 +34,7 @@
 
     <div class="mb-3 task">
         <div class="mt-3 mb-3">
+            <span class="sel-sub-header h5" style="background-color: #88df6c;">@lang('Request')</span>
             <label for="exampleFormControlTextarea1" class="form-label">@lang('Please share what you think the protagonist has observed from the highlighted sentence (Request)')</label>
             <textarea class="form-control" id="exampleFormControlTextarea1" name="nvc_4" rows="4">@isset($student_answer){{ $student_answer->nvc_4 }}@endisset</textarea>
         </div>
